e56c5d4d41fff5df9b211350cd46bcaba0d93d6a
GeoCod <artem.g.vasilchenko@gmail.com>
2020-04-16 01:01:34 +0300

    Урок 5, задание 1а

--- Lesson01/Program.cs
+++ Lesson01/Program.cs
@@ -1,47 +1,68 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using static View;
 
 public class Program
 {
-    private static void Main()
-    //Васильченко Артем
+    struct Element
     {
-        #region Урок 4
+        public string FIO;
+        public int N;
+    }
 
-        Print("Заадние 1");
-        MyArray Mass = new MyArray(20, -10000, 10000);
-        Print(Mass.CountCoupleDivThree());
+    private static void Main()
+	//Васильченко Артем
+	{
+
+        Console.WriteLine("Задание 1.а");
+        Console.WriteLine("Введите логин:");
+        Login login = new Login();
+        if (login.PasswordCheck(Console.ReadLine()))
+            Print("Корректный");
+        else
+            Print("Ошибка");
         Pause();
 
-        //Задание 2
+        return;
+        StreamReader sr = new StreamReader("data.txt");
+        int N = int.Parse(sr.ReadLine());
+        Element[] a = new Element[N];
+        for (int i = 0; i < N; i++)
+        {
+            string[] s = sr.ReadLine().Split(' ');
+            a[i].FIO = s[0] + s[1];
+            a[i].N = int.Parse(s[2]);
+        }
+        sr.Close();
+        int[] massiv = new int[100];
+        for (int i = 0; i < N; i++) massiv[a[i].N]++;
+        int min = 100;
+        for (int i = 0; i < 100; i++)
+            if (massiv[i] != 0 && massiv[i] < min) min = massiv[i];
+        for (int i = 0; i < 100; i++)
+            if (massiv[i] == min) Console.WriteLine(i);
+        Console.ReadKey();
 
-
-        Print("Заадние 3");
-        MyArray Mass2 = new MyArray(10, -355, 15, 'R');     // Как лучше было сделать два похожих конструктора? Есть мин-макс и с шагом
-        Print(Mass2.Sum);
-        Pause();
-        Print("");
-
-        Print("--метод Inverse");
-        Mass2.Inverse();
-        Print(Mass2.ToString());
-        Pause();
-        Print("");
-
-        Print("--метод Multi");
-        Mass2.Multi(10);
-        Print(Mass2.ToString());
-        Pause();
-        Print("");
-
-        Print("Задание 4");
-        MyArray2d Mass2d = new MyArray2d(3, 5, 0, 500);
-        int X, Y;
-        Print(Mass2d.ToString());
-        Mass2d.CoordMaxNum(out X, out Y);
-        Print(X); Print(Y);
         Pause();
 
-        #endregion
+
+        /*
+
+            string poems = "белеет парус одинокий в тумане моря голубом";
+                char[] div = { ' ' }; // создаем массив разделителей
+                                      // разбиваем строку на части,
+                string[] parts = poems.Split(div);
+                Console.WriteLine("Результат разбиения строки на части: ");
+                for (int i = 0; i < parts.Length; i++)
+                    Console.WriteLine(parts[i]);
+                // собираем эти части в одну строку, в качестве разделителя используем символ |
+                string str = String.Join("|", parts);
+                Console.WriteLine("Результат сборки: ");
+                Console.WriteLine(str);
+                Pause();
+        */
+
+
     }
 }
