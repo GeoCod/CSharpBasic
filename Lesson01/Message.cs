5f4817f9924656b1eeb6e1cd846fe74903bbe486
GeoCod <artem.g.vasilchenko@gmail.com>
2020-04-17 00:14:03 +0300

    Выполнил 3 задания

---
+++ Lesson01/Message.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+static class Message
+{
+    //2. Разработать статический класс Message, содержащий следующие статические методы для обработки текста:
+    //а) Вывести только те слова сообщения, которые содержат не более n букв.
+    /// <summary>
+    /// Вывод слов соответствующей длины
+    /// </summary>
+    /// <param name="str">Проверяемый текст</param>
+    /// <param name="limit">Количество символов</param>
+    /// <returns>Строка из соответствующих слов</returns>
+    public static string LetterLimit(string str, int limit)
+    {
+        string[] words = SeparationString(str);
+        var list = new List<string>();
+
+        string pattern = @"\b(\w{" + Regex.Escape(limit.ToString()) + @"})\b";
+        Regex regex = new Regex(pattern);
+        for (int i = 0; i < words.Length; ++i)
+        {
+            if (regex.IsMatch(words[i])) list.Add(words[i]);
+        }
+
+        string strOut = "";
+        foreach (string item in list)
+        {
+            strOut += item + " ";
+        }
+        return strOut;
+    }
+
+    /// <summary>
+    /// б) Удаляет из сообщения все слова, которые заканчиваются на заданный символ
+    /// </summary>
+    /// <param name="str">Входная строка</param>
+    /// <param name="sym">Искомый символ</param>
+    public static string DeleteWord(string str, char sym)
+    {
+        string[] words = SeparationString(str);
+        var list = new List<string>();
+        string pattern = $@"\w *[{sym}]\b";
+        Regex regex = new Regex(pattern);
+        for (int i = 0; i < words.Length; ++i)
+        {
+            if (!regex.IsMatch(words[i])) list.Add(words[i]);
+        }
+
+        string strOut = "";
+        foreach (string item in list)
+        {
+            strOut += item + " ";
+        }
+        return strOut;
+    }
+
+    //в) Найти самое длинное слово сообщения.
+    /// <summary>
+    /// Поиск самого длинного слова в строке
+    /// </summary>
+    public static string LongWord(string str)
+    {
+        string[] words = SeparationString(str);
+        int maxLength = 0;
+        string maxWord = "";
+        foreach (string w in words)
+        {
+            if (w.Length >= maxLength)
+            {
+                maxLength = w.Length;
+                maxWord = w;
+            }
+        }
+        return maxWord;
+    }
+
+    //г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+    /// <summary>
+    /// Формирование строки из самых длинных слов
+    /// </summary>
+    /// <param name="str">Входная строка</param>
+    /// <returns>Строка</returns>
+    public static string stringFromLongWord(string str)
+    {
+        StringBuilder sb = new StringBuilder();
+        string[] words = SeparationString(str);
+        int maxLength = 0;
+        foreach (string w in words)
+            maxLength = w.Length >= maxLength ? w.Length : maxLength;
+
+        foreach (string w in words)
+            if (w.Length == maxLength)
+                sb.Append(w + ", ");
+
+        return sb.ToString();
+    }
+
+    /* д) *** Создать метод, который производит частотный анализ текста.
+    //В качестве параметра в него передается массив слов и текст, в качестве результата метод возвращает сколько раз каждое из слов массива входит в этот текст.
+    //Здесь требуется использовать класс Dictionary.
+    */
+    /// <summary>
+    /// Частотный анализ текста
+    /// </summary>
+    /// <param name="words">Массив искомых слов</param>
+    /// <param name="text">Проверяемый текст</param>
+    /// <returns>Пары слово - количество</returns>
+    public static string WordFrequency(string[] words, string text)
+    {
+        Dictionary<string, int> wordCount = new Dictionary<string, int>();
+        string[] wordsText = SeparationString(text);
+
+        foreach (string w in words)
+            wordCount.Add(w, 0);
+        foreach (string w in wordsText)
+            if (wordCount.ContainsKey(w))
+                ++wordCount[w];
+
+        string outText = "";
+        foreach (var item in wordCount)
+            outText += item.Key + " - " + item.Value + " | ";
+        return outText;
+    }
+
+    /// <summary>
+    /// Конвертация строки в массив строк
+    /// </summary>
+    /// <param name="str">Входная строка</param>
+    /// <returns>Массив строк</returns>
+    private static string[] SeparationString(string str)
+    {
+        char[] div = { ' ' };
+        string[] word = str.Split(div);
+        return word;
+    }
+
+
+    //3. *Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой.
+    /// <summary>
+    /// Проверка на перестановку символов
+    /// </summary>
+    /// <param name="text1">первая строка</param>
+    /// <param name="text2">вторая строка</param>
+    /// <returns>true или false</returns>
+    public static bool IsReverse(string text1, string text2)
+    {
+        bool check = false;
+        if (text1 == null || text2 == null) return check;
+        else if ((text1.Length != text2.Length) || (text1 != text2))
+        {
+            // переводим первую строку в массив символов и загоняем их в словарь с подсчетом количества
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+            foreach (char c in text1.ToCharArray())
+                if (charCount.ContainsKey(c))
+                    ++charCount[c];
+                else
+                    charCount.Add(c, 1);
+
+            // перебираем символы второй строки и уменьшаем их количество в словаре
+            foreach (char c2 in text2.ToCharArray())
+                if (charCount.ContainsKey(c2))
+                    --charCount[c2];
+                else
+                {
+                    return check;
+                }
+            // если в словаре все ключи = 0, значит строки являются перестановками
+            foreach (int i in charCount.Values)
+                if (i == 0)
+                    check = true;
+                else
+                {
+                    return check;
+                }
+            return check;
+        }
+        else
+            return check;
+
+    }
+}
+
--- Lesson01/Program.cs
+++ Lesson01/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Text;
 using static View;
 
 public class Program
@@ -12,66 +11,93 @@ public class Program
     }
 
     private static void Main()
-	//Васильченко Артем
-	{
-
-        Console.WriteLine("Задание 1.а");
+    //Васильченко Артем
+    {
+        Console.WriteLine("Задание 1.а.\nПрвоерка логина без рекурсии");
         Console.WriteLine("Введите логин:");
         Login login = new Login();
         if (login.PasswordCheck(Console.ReadLine()))
             Print("Корректный");
         else
-            Print("Ошибка\n");
+            Print("Ошибка");
         Pause();
 
-        Console.WriteLine("Задание 1.б");
+        //----------------------------------------------
+        Console.WriteLine("\nЗадание 1.б.\nПрвоерка логина с рекурсией");
         Console.WriteLine("Введите логин:");
         if (login.PasswordCheckRegex(Console.ReadLine()))
             Print("Корректный");
         else
             Print("Ошибка");
         Pause();
-        
+        //-----------------------------------------------
 
-        return;
-        StreamReader sr = new StreamReader("data.txt");
-        int N = int.Parse(sr.ReadLine());
-        Element[] a = new Element[N];
-        for (int i = 0; i < N; i++)
-        {
-            string[] s = sr.ReadLine().Split(' ');
-            a[i].FIO = s[0] + s[1];
-            a[i].N = int.Parse(s[2]);
-        }
-        sr.Close();
-        int[] massiv = new int[100];
-        for (int i = 0; i < N; i++) massiv[a[i].N]++;
-        int min = 100;
-        for (int i = 0; i < 100; i++)
-            if (massiv[i] != 0 && massiv[i] < min) min = massiv[i];
-        for (int i = 0; i < 100; i++)
-            if (massiv[i] == min) Console.WriteLine(i);
-        Console.ReadKey();
+        Print("\nЗадание 2.а.\nВыбор слов соответствующей длины");
+        Print("Введите количество символов:");
+        int limit = int.Parse(Console.ReadLine());
 
+        Print("Введите предложение:");
+        Print("Подходящие слова: " + Message.LetterLimit(Console.ReadLine(), limit));
+        Pause();
+        //-----------------------------------------------
+
+        Print("\nЗадание 2.б.\nУдаление слов оканчивающиеся на указанный символ");
+        Print("Введите символ:");
+        char sym = char.Parse(Console.ReadLine());
+
+        Print("Введите слова:");
+        Print(Message.DeleteWord(Console.ReadLine(), sym));
+        Pause();
+        //-----------------------------------------------
+
+        Print("\nЗадание 2.в.\nПоиск самого длинного слова.\nВведите слова:");
+        Print("Это слово - " + Message.LongWord(Console.ReadLine()));
+        Pause();
+        //-----------------------------------------------
+
+        Print("Строка из длинных слов\nВведите слова через пробел:");
+        Print("Новая строка:\n" + Message.stringFromLongWord(Console.ReadLine()));
+        Pause();
+        //-----------------------------------------------
+
+        Print("\nЗадание 2.г.\nПодсчет частоты вхождения слов.");
+        string[] words = { "1", "2", "3", "4" };
+        string text = "1 1 1 2 2 2 2 2 3 3";
+        Print("Новая строка:\n" + Message.WordFrequency(words, text));
+        Pause();
+        //-----------------------------------------------
+
+        Print("\nЗадание 2.д.\nПроверка текста на перестановку.\nВведите первое сообщение");
+        string text1 = Console.ReadLine();
+        Print("Введите второе сообщение:");
+        string text2 = Console.ReadLine();
+        string msg = Message.IsReverse(text1, text2) ? "это перестановка" : "это разные строки";
+        Print(msg);
         Pause();
 
 
-        /*
+        void Test()
+        {
+            StreamReader sr = new StreamReader("data.txt");
+            int N = int.Parse(sr.ReadLine());
+            Element[] a = new Element[N];
+            for (int i = 0; i < N; i++)
+            {
+                string[] s = sr.ReadLine().Split(' ');
+                a[i].FIO = s[0] + s[1];
+                a[i].N = int.Parse(s[2]);
+            }
+            sr.Close();
+            int[] massiv = new int[100];
+            for (int i = 0; i < N; i++) massiv[a[i].N]++;
+            int min = 100;
+            for (int i = 0; i < 100; i++)
+                if (massiv[i] != 0 && massiv[i] < min) min = massiv[i];
+            for (int i = 0; i < 100; i++)
+                if (massiv[i] == min) Console.WriteLine(i);
+            Console.ReadKey();
 
-            string poems = "белеет парус одинокий в тумане моря голубом";
-                char[] div = { ' ' }; // создаем массив разделителей
-                                      // разбиваем строку на части,
-                string[] parts = poems.Split(div);
-                Console.WriteLine("Результат разбиения строки на части: ");
-                for (int i = 0; i < parts.Length; i++)
-                    Console.WriteLine(parts[i]);
-                // собираем эти части в одну строку, в качестве разделителя используем символ |
-                string str = String.Join("|", parts);
-                Console.WriteLine("Результат сборки: ");
-                Console.WriteLine(str);
-                Pause();
-        */
-
-
+            Pause();
+        }
     }
 }
