1b230b9dbba64b4a199af311aa98017cdaa89fb4
GeoCod <artem.g.vasilchenko@gmail.com>
2020-04-20 11:01:12 +0300

    Выполнил 1 задание 7 урока

---
+++ Less-07/Numeric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Less_07
+{
+    public class Numeric
+    {
+        // в) *Добавить кнопку «Отменить», которая отменяет последние ходы.Используйте обобщенный класс Stack.
+        // Вся логика игры должна быть реализована в классе с удвоителем.
+        private int num;
+        Random rnd = new Random();
+        public int RandomNum
+        {
+            get { return num = rnd.Next(100); }
+        }
+
+    }
+}
